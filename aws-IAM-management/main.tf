6fb1992f214804eaba2845ebf7030b66b57cc2a9
grishma2477 <kandelgrishma2477@gmail.com>
2026-06-08 13:40:23 +1000

    AWS IAM Management Completed

--- aws-IAM-management/main.tf
+++ aws-IAM-management/main.tf
@@ -13,10 +13,17 @@ provider "aws" {
 
 locals {
   users_data = yamldecode(file("./users.yaml")).users
+
+  user_role_pair = flatten(
+    [for user in local.users_data: [for role in user.roles: {
+    username = user.username
+    role = role
+  }]]
+  )
 }
 
 output "output" {
-  value = local.users_data[*].username
+  value = local.user_role_pair
 }
 
 #creating users 
@@ -38,4 +45,18 @@ resource "aws_iam_user_login_profile" "profile" {
         pgp_key,
        ]
     }
+}
+
+#Attaching policies 
+resource "aws_iam_user_policy_attachment" "main" {
+    for_each = {
+     for pair in local.user_role_pair :
+     "${pair.username}-${pair.role}" => pair 
+    }
+
+    #baburao-EC2Access = {username = baburao, role= ec2access}
+    #baburao-s3ready = {}
+
+    user = aws_iam_user.main[each.value.username].name
+    policy_arn = "arn:aws:iam::aws:policy/${each.value.role}"
 }
