46c33f2b95949388cca9a951a2b59d39cd276f28
grishma2477 <kandelgrishma2477@gmail.com>
2026-06-08 17:00:36 +1000

    Creating Own terraform Module

---
+++ tf-own-module-VPC/module/vpc/outputs.tf
@@ -0,0 +1,23 @@
+#VPC 
+
+output "vpc_id" {
+    value = aws_vpc.main.id
+}
+
+locals {
+    # To format the subnet IDs which may be multiples in format of subnet_name = {id=, az=}
+    public_subnet_output = {
+        for key, config in local.public_subnet: key => {
+            subnet_id = aws_subnet.main[key].id 
+            az = aws_subnet.main[key].availability_zone 
+        }
+    }
+}
+
+output "public_subnets" {
+  value = ""
+}
+
+output "private_subnet" {
+    value = ""
+}
